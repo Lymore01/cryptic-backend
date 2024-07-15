d7d6b02e89e25ab7ca74402c12bffdc328459d28
Kelly Limo <130097627+Lymore01@users.noreply.github.com>
2024-07-15 12:41:42 +0300

    b

--- index.js
+++ index.js
@@ -1,46 +1,41 @@
-require("dotenv").config()
-require("./database/config/index")
-const express = require("express")
+require("dotenv").config();
+require("./database/config/index");
+const express = require("express");
 const session = require("express-session");
 const store = require("./database/SessionStorage/sessions");
-const PORT = process.env.PORT
-const bodyParser = require('body-parser')
-const cors = require("cors")
-const userRoute = require("./routes/users/usersRoute")
-const keysRoute = require("./routes/keys/createKeys")
+const PORT = process.env.PORT;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const userRoute = require("./routes/users/usersRoute");
+const keysRoute = require("./routes/keys/createKeys");
 
-const app = express()
+const app = express();
 
 app.use(
-    session({
-      secret: process.env.SESSION_SECRET_KEY,
-      resave: false,
-      saveUninitialized: false,
-      store: store,
-      cookie: {
-        maxAge: 1200000, //20 mins
-      },
-    })
-  );
-
+  session({
+    secret: process.env.SESSION_SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+    cookie: {
+      maxAge: 1200000, //20 mins
+    },
+  })
+);
 
 //   middlewares
-app.use(bodyParser.json())
-app.use(cors({
-  origin:"http://localhost:5173"
-}))
+app.use(bodyParser.json());
+app.use(cors());
 
+app.use("/api", userRoute);
+app.use("/key", keysRoute);
 
+app.get("/", (req, res) => {
+  res.send("hello");
+});
 
-app.use("/api" ,userRoute)
-app.use("/key" ,keysRoute)
-
-app.get("/", (req,res)=>{
-    res.send("hello")
-})
-
-app.listen(PORT, ()=>{
-    console.log("Listening on port 3000")
-})
+app.listen(PORT, () => {
+  console.log("Listening on port 3000");
+});
 
 module.exports = app;
